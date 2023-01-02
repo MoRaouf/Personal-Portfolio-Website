c5a91778de5e86ede840723e3989ba5f69af6a71
Mohammed Raouf <78295016+MoRaouf@users.noreply.github.com>
2023-01-02 11:15:44 +0400

    Update EducationCard.js

--- src/components/educationCard/EducationCard.js
+++ src/components/educationCard/EducationCard.js
@@ -53,7 +53,9 @@ export default function EducationCard({school}) {
                 {school.duration}
               </p>
               <p className="education-text-desc">{school.desc}</p>
-              <div className="education-text-bullets">
+              <div className={`${
+                  isDark ? "dark-mode" : ""
+                } education-text-bullets`>
                 <ul>
                   <GetDescBullets descBullets={school.descBullets} />
                 </ul>
